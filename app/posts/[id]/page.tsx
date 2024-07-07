850f8f2e37fb9718ee8445559eff9414326c8e5c
taiebhossain <taieb.hossain006@gmail.com>
2024-07-07 08:18:07 +0600

    change post desc to content

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -15,7 +15,7 @@ export default async function PostdetailsPage({
     <div className='flex flex-col items-center p-20'>
       <div className='rounded p-8'>
         <h2 className='font-bold text-3xl mb-5'>{post?.title}</h2>
-        <p>{post?.desc}</p>
+        <p>{post?.content}</p>
       </div>
     </div>
   );
